1f10b825048eb91ccd507da0a249e3226ab4583b
TIm St.Clair <tim.stclair@gmail.com>
2020-06-03 14:31:28 +1000

    version bump

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020041904;
+$plugin->version = 2020041905;
 $plugin->requires = 2019111800;
 $plugin->release = '3.8.0.0';
 $plugin->component = 'auth_oidc';
